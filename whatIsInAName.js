5b63fad4e2fc4c00d7e0bb2d132d1a96844ce881
Miguel Prada <mapra99@gmail.com>
2020-05-30 21:18:59 -0500

    what is in a name

--- whatIsInAName.js
+++ whatIsInAName.js
@@ -1,1 +1,18 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou
+
+function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+  arr = collection.filter(object => {
+    let includesAll = true;
+    Object.keys(source).forEach(key => {
+      includesAll = includesAll && object[key] === source[key];
+    })
+
+    return(includesAll)
+  })
+  // Only change code above this line
+  return arr;
+}
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
